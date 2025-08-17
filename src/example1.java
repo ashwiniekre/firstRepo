c4d230524551727de194333dc50983a4138c408f
ashwiniekre <bobadeashwini92@gmail.com>
2025-08-17 19:13:56 +0530

    Update example1.java
    updated the program

--- src/example1.java
+++ src/example1.java
@@ -2,12 +2,20 @@ public class example1{
 	
 	public static void main() {
 		
+		example1 cal = new example1();
+		 cal.calculate();
+		
+		
 	}
 	
 	public void calculate() {
 		
-		//this function will calculate simple interest
+		//this function will calculate simple interest 
+		System.out.println("calculating interest");
 		
 		
 	}
+	
+	
+	
 }
